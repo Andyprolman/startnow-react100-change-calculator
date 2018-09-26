be6bce8e50462282dcb909651571a5330f77805a
Andrew Prolman <aprolman21@gmail.com>
2018-09-26 14:59:30 -0700

    fnished logic and added danger alert. works

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,17 @@ class App extends Component {
     this.state = {
       amountDue: 0,
       amountReceived: 0,
-      alert: ''
+      alertSuccess: '',
+      alertDanger: '',
+      twenties: 0,
+      tens: 0,
+      fives: 0,
+      ones: 0,
+      quarters: 0,
+      dimes: 0,
+      nickels: 0,
+      pennies: 0,
+      success: true,
     }
     this.changeValues = this.changeValues.bind(this);
     this.calculate = this.calculate.bind(this);
@@ -21,19 +31,35 @@ class App extends Component {
 
   calculate(e){
     e.preventDefault()
-    const change = this.state.amountReceived - this.state.amountDue;
-    this.setState({
-      alert: `The total change due is $${change}.`
-    })
+    const change = (this.state.amountReceived - this.state.amountDue).toFixed(2);
+    const negChange = change*(-1);
+    const twenties = Math.floor(change/20);
+    const tens = Math.floor((change-(twenties*20))/10);
+    const fives = Math.floor((change-((twenties*20)+(tens*10)))/5);
+    const ones = Math.floor(change-((twenties*20)+(tens*10)+(fives*5)))
+    const coin = (change-Math.floor(change)).toFixed(2);
+    const quarters = Math.floor(coin/.25);
+    const dimes = Math.floor((coin-(quarters*.25))/.10)
+    const nickels = Math.floor((coin-((quarters*.25)+(dimes*.10)))/.05)
+    const pennies = Math.round((coin-((quarters*.25)+(dimes*.10)+(nickels*.05)))/.01);
+    this.setState({ 
+      success: (change >= 0) ? true : false, 
+      alertSuccess: `The total change due is $${change}.`,
+      alertDanger: `Additional $${negChange} still owed`,
+      twenties: twenties,
+      tens: tens,
+      fives: fives,
+      ones: ones, 
+      quarters: quarters,
+      dimes: dimes,
+      nickels: nickels,
+      pennies: pennies,
+        
+    });
 
-    console.log(change);
-    console.log(this.state);
   }
   
-  render() {
-
-
-
+  render(){
     var panel = {
       borderLeftWidth: '0px',
       paddingLeft: '2px',
@@ -98,30 +124,33 @@ class App extends Component {
             </div>
           </div>
           <div className='col-md-8 well'>
-            <div className='alert alert-success'>{this.state.alert}</div>
+            <div className='alerts' style={{textAlign: 'center', fontWeight: 'bold'}}>
+              <div className='alert alert-success' hidden={!this.state.success}>{this.state.alertSuccess}</div>
+              <div className='alert alert-danger' hidden={this.state.success} >{this.state.alertDanger}</div>
+            </div>
             <div className='row'>
               <div className='col-md-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='twenties' style={{fontWeight: 'bold'}}>Twenties</h4>
-                  <p className='num-twenties'>0</p>
+                  <p name='twenties'className='num-twenties'>{this.state.twenties}</p>
                 </div>
               </div>
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='tens' style={{fontWeight: 'bold'}}>Tens</h4>
-                  <p className='num-tens'>0</p>
+                  <p name='tens' className='num-tens'>{this.state.tens}</p>
                 </div>
               </div>
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='fives' style={{fontWeight: 'bold'}}>Fives</h4>
-                  <p className='num-fives'>0</p>
+                  <p name='fives' className='num-fives'>{this.state.fives}</p>
                 </div>
               </div>
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='ones' style={{fontWeight: 'bold'}}>Ones</h4>
-                  <p className='num-ones'>0</p>
+                  <p name='ones' className='num-ones'>{this.state.ones}</p>
                 </div>
               </div>
             </div>
@@ -129,25 +158,25 @@ class App extends Component {
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='quarters' style={{fontWeight: 'bold'}}>Quarters</h4>
-                  <p className='num-quarters'>0</p>
+                  <p name='quarters' className='num-quarters'>{this.state.quarters}</p>
                 </div>
               </div>
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='dimes' style={{fontWeight: 'bold'}}>Dimes</h4>
-                  <p className='num-dimes'>0</p>
+                  <p name='dimes' className='num-dimes'>{this.state.dimes}</p>
                 </div>
               </div>
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='nickels' style={{fontWeight: 'bold'}}>Nickels</h4>
-                  <p className='num-nickels'>0</p>
+                  <p name='nickels' className='num-nickels'>{this.state.nickels}</p>
                 </div>
               </div>
               <div className='col-sm-3'>
                 <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
                   <h4 className='pennies' style={{fontWeight: 'bold'}}>Pennies</h4>
-                  <p className='num-pennies'>0</p>
+                  <p name='pennies' className='num-pennies'>{this.state.pennies}</p>
                 </div>
               </div>
             </div>
