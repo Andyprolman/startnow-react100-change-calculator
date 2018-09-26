1ddc0011a4cc414383435d18a49084340481aaa8
Andrew Prolman <aprolman21@gmail.com>
2018-09-25 17:32:04 -0700

    finished up styling and started logic. alert message works.

--- src/app.jsx
+++ src/app.jsx
@@ -3,50 +3,153 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      amountDue: 0,
+      amountReceived: 0,
+      alert: ''
+    }
+    this.changeValues = this.changeValues.bind(this);
+    this.calculate = this.calculate.bind(this);
+  }
+
+  changeValues(e){
+    console.log(e.target.name, e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  calculate(e){
+    e.preventDefault()
+    const change = this.state.amountReceived - this.state.amountDue;
+    this.setState({
+      alert: `The total change due is $${change}.`
+    })
+
+    console.log(change);
+    console.log(this.state);
   }
   
   render() {
+
+
+
+    var panel = {
+      borderLeftWidth: '0px',
+      paddingLeft: '2px',
+      height: '222px',
+    }
+
+    var panelBody = {    
+      padding: '5px',
+    }
+
+    var label1 = {
+      padding: '0 10px 10px 10px',
+      width: '337 px',
+    }
+
+    var label2 = {
+      padding: '10px 10px 10px 10px',
+      width: '337 px',
+    }
+
+    var input = {
+      padding: '0 80px',
+      marginLeft: '10px',
+      marginRight: '5px',
+    }
+
+    var button = {
+      margin: '10px',
+      width: '316px',
+    }
     return(
       <div className='container'>
         <div id='header'>
-          <h1>Change Calculator</h1>
+          <h1 style={{color:'white'}}>Change Calculator</h1>
         </div>
-        <div id='tagline'>
-        </div>
+        <hr id='tagline' style={{width: '1153px'}}>
+        </hr>
         <div className='row'>
           <div className='col-md-4'>
-            <div className='panel panel-default'>
+            <div className='panel panel-default' style={panel}>
               <div className='panel-heading'>Enter information.</div>
-              <div className='panel-body container'></div>
-                <form>
+              <div className='panel-body container' style={panelBody}></div>
+                <form onSubmit={this.calculate}>
                   <div className='form-group'>
-                    <div>
-                      <label for='amountDue'>How much is due?</label>
+                    <div style={label1}>
+                      <label htmlFor='amountDue'>How much is due?</label>
                     </div>
                     <div>
-                      <input name='amountDue' type='number' step='0.01'/>
+                      <input name='amountDue' type='number' step='0.01' style={input} onChange={this.changeValues}/>
+                    </div>
+                    <div style={label2}>
+                      <label htmlFor='amountReceived'>How much was received?</label>
                     </div>
                     <div>
-                      <label for='amountReceived'>How much was received?</label>
+                      <input name='amountReceived' type='number' step='0.01' style={input} onChange={this.changeValues}/>
                     </div>
                     <div>
-                      <input name='amountReceived' type='number' step='0.01'/>
-                    </div>
-                    <div>
-                      <button name='button'  className='btn-primary'>Calulate</button>
+                      <button name='button' type='submit' className='btn-primary' style={button}>Calulate</button>
                     </div>
                   </div>
                 </form>
             </div>
           </div>
-          <div className='col-md-8 well well-lg'>
-            <div className='alert alert-success'>You owe this much.</div>
+          <div className='col-md-8 well'>
+            <div className='alert alert-success'>{this.state.alert}</div>
             <div className='row'>
-              <div className='well well-md'> Twenties</div>
-              <div className='well well-md'> Tens</div>
-              <div className='well well-md'> Fives</div>
-              <div className='well well-md'> Ones</div>
-
+              <div className='col-md-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='twenties' style={{fontWeight: 'bold'}}>Twenties</h4>
+                  <p className='num-twenties'>0</p>
+                </div>
+              </div>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='tens' style={{fontWeight: 'bold'}}>Tens</h4>
+                  <p className='num-tens'>0</p>
+                </div>
+              </div>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='fives' style={{fontWeight: 'bold'}}>Fives</h4>
+                  <p className='num-fives'>0</p>
+                </div>
+              </div>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='ones' style={{fontWeight: 'bold'}}>Ones</h4>
+                  <p className='num-ones'>0</p>
+                </div>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='quarters' style={{fontWeight: 'bold'}}>Quarters</h4>
+                  <p className='num-quarters'>0</p>
+                </div>
+              </div>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='dimes' style={{fontWeight: 'bold'}}>Dimes</h4>
+                  <p className='num-dimes'>0</p>
+                </div>
+              </div>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='nickels' style={{fontWeight: 'bold'}}>Nickels</h4>
+                  <p className='num-nickels'>0</p>
+                </div>
+              </div>
+              <div className='col-sm-3'>
+                <div className='well' style={{backgroundColor:'#e6e6e6',textAlign:'center'}}>
+                  <h4 className='pennies' style={{fontWeight: 'bold'}}>Pennies</h4>
+                  <p className='num-pennies'>0</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
